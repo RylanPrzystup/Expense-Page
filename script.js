37ae92637852e12f7f2d66ff5518d0f5bf555288
RylanPrzystup <rylanprzystup9@gmail.com>
2026-02-20 14:27:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let expenses = [];
+function addExpense() {
+ 
+  let amount = document.getElementById("amountInput").value; 
+  let name = document.getElementById("expenseInput").value;
+  let category = document.getElementById("categoryinput").value;
+  if (!name || !amount || !category) {
+    return;
+  }
+
+  let newExpense = {
+    name: name,
+    amount: Number(amount),
+    category: category,
+  };
+  expenses.push(newExpense);
+  displayExpenses();
+}
+
+function displayExpenses() {
+  let total = document.getElementById("total");
+  let table = document.getElementById("table");
+  table.innerHTML = "";
+  let totalAmount = 0;
+
+  let html = `<tr>
+                    <th>Expense Name</th>
+                    <th>Amount</th>
+                    <th>Catagory</th>
+                    <th>Action</th>
+                </tr>`;
+
+  for (let i = 0; i < expenses.length; i++) {
+    totalAmount += expenses[i].amount;
+    html += `<tr>
+        <td>${expenses[i].name}</td>
+        <td>${expenses[i].amount}</td>
+        <td>${expenses[i].category}</td>
+        <td>
+            <button onclick="editExpense(${i})">Edit</button>
+            <button onclick="deleteExpense(${i})">Delete</button>
+        </td>
+        </tr>`;
+  }
+
+  html += `</table>`;
+  table.innerHTML = html;
+  total.innerHTML = `Total: $${totalAmount}`;
+}
+
+function editExpense(index) {
+  let newName = prompt("Expense Name: ", expenses[index].name);
+  let newAmount = prompt("Expense Amount: ", expenses[index].amount);
+  let newCategory = prompt("Expense Category: ", expenses[index].category);
+
+  if (!newName || !newAmount || !newCategory) {
+    return;
+  }
+
+  if (
+    newCategory !== "food" ||
+    newCategory !== "transport" ||
+    newCategory !== "entertainment" ||
+    newCategory !== "other"
+  ) {
+    return;
+  }
+
+  expenses[index].name = newName;
+  expenses[index].amount = Number(newAmount);
+  expenses[index].category = newCategory;
+  displayExpenses();
+}
+
+function deleteExpense(index) {
+  expenses.splice(index, 1);
+  displayExpenses();
+}
